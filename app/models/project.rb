304a1f98163b943bbcc0a953c288d6df9d02f165
Abdullah <abd00s@users.noreply.github.com>
2014-12-06 17:34:07 -0500

    make views for all project actions

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ActiveRecord::Base
   has_many :rewards
   # has_many :comments
   has_many :pledges, through: :rewards
+
+  def formatted_start_date
+    start_date.strftime("%b %d, %Y")
+  end
+
+  def formatted_end_date
+    end_date.strftime("%b %d, %Y")
+  end
 end
