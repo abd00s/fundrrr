8ab0cf885e5fa3c67a1d30e4116e3a876b801689
Abdullah <abd00s@users.noreply.github.com>
2014-12-06 12:32:24 -0500

    g Reward model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206172411) do
+ActiveRecord::Schema.define(version: 20141206173105) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20141206172411) do
     t.datetime "end_date"
     t.integer  "funding_goal"
     t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rewards", force: true do |t|
+    t.integer  "price_tier"
+    t.text     "description"
+    t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
