cefb570b174c25156779fa05837a0bbffb90b993
Abdullah <abd00s@users.noreply.github.com>
2014-12-06 20:01:12 -0500

    make views for user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
 
   private 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmtion)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
   def set_user
