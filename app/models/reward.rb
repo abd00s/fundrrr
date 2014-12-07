9a83dc8033312ce86cf41efa5cb65ebeaad7e925
Abdullah <abd00s@users.noreply.github.com>
2014-12-06 21:04:03 -0500

    user can see if theyve backed project, at what tier, and link for it is disbaled

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,4 +1,10 @@
 class Reward < ActiveRecord::Base
   belongs_to :project
   has_many :pledges
+
+  def backers
+    backers = []
+    pledges.each {|pledge| backers << pledge.backer}
+    backers
+  end
 end
