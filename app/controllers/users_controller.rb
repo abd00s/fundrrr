8c0e73ba781cd8e5bd20bd1ff472033b699f5a56
Abdullah <abd00s@users.noreply.github.com>
2014-12-06 19:29:38 -0500

    set users controller actions and user routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Success! Welcome to fundRRR"
+    else
+      render 'new', notice: "Error signing up, try again!"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user), notice: "Changes successful."
+    else
+      render 'edit', notice: "Error, try again!"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path, notice: "Account succesfully deleted."
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmtion)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
